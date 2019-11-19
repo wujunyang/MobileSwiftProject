85472b66fb8cad1d2b2da6ce69cce0105a672361
wujunyang <wujunyang@126.com>
2019-11-19 17:16:18 +0800

    add project, import pod

---
+++ MobileSwiftProject/MobileSwiftProject/MVPHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MVPHomeViewController.swift
+//  MobileSwiftProject
+//
+//  Created by wujunyang on 2019/11/19.
+//  Copyright © 2019 com.sdms. All rights reserved.
+//
+
+import UIKit
+
+class MVPHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor=UIColor.blue;
+        
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MobileSwiftProject/Pods/Target Support Files/Pods-MobileSwiftProject/Pods-MobileSwiftProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileSwiftProject : NSObject
+@end
+@implementation PodsDummy_Pods_MobileSwiftProject
+@end
