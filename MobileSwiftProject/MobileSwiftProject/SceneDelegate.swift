3ea6834bb14bf11616999682b68becbbab69edf3
wujunyang <wujunyang@126.com>
2019-12-10 18:45:45 +0800

    Alamofire运用

--- MobileSwiftProject/MobileSwiftProject/SceneDelegate.swift
+++ MobileSwiftProject/MobileSwiftProject/SceneDelegate.swift
@@ -26,7 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             //window.rootViewController = UIHostingController(rootView: contentView)
-            let homeVc=MVPKeyWordViewController();
+            let homeVc=MVPHomeViewController();
             let nav=UINavigationController.init(rootViewController: homeVc);
             window.rootViewController = nav;
             self.window = window
